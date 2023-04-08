e0f42f71a55c7b7efcbad09bb4a60692c3457030
Muhammad Fareed <muhammed.fareed.94@gmail.com>
2023-04-09 01:50:24 +0200

    Creating photso repository

--- PhotosRetriever/PhotosRetriever/PhotosModule/Repository/PhotosRepositroy.swift
+++ PhotosRetriever/PhotosRetriever/PhotosModule/Repository/PhotosRepositroy.swift
@@ -14,3 +14,17 @@ enum PhotosRepositoryError: Error {
 protocol PhotosRepositoryProtocol: AnyObject {
     func fetchPhotosList(pageNumber: Int, onSuccess: @escaping ([Photo]) -> Void, onFailure: @escaping (PhotosRepositoryError) -> Void)
 }
+
+class PhotosRepository: PhotosRepositoryProtocol {
+    let remoteDataStore: PhotosDataStoreProtocol
+    
+    init(withRemoteDataStore remoteDataStore: PhotosDataStoreProtocol = RemotePhotosDataStore()) {
+        self.remoteDataStore = remoteDataStore
+    }
+    
+    func fetchPhotosList(pageNumber: Int, onSuccess: @escaping ([Photo]) -> Void, onFailure: @escaping (PhotosRepositoryError) -> Void) {
+        remoteDataStore.fetchPhotosList(pageNumber: pageNumber, onSuccess: onSuccess, onFailure: { error in
+            onFailure(PhotosRepositoryError.noData)
+        })
+    }
+}
