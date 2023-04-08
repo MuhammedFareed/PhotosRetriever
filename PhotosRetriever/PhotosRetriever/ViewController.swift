0e080d05750fad5ff1389088439172613d2133b5
Muhammad Fareed <muhammed.fareed.94@gmail.com>
2023-04-08 23:48:56 +0200

    Initiating project from xcode

---
+++ PhotosRetriever/PhotosRetriever/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotosRetriever
+//
+//  Created by Muhammad Fareed on 08/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
