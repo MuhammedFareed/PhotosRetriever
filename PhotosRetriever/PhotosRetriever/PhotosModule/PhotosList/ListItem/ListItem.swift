beee0ce1bb1a249fe023a9f06da9b26899cc626f
Muhammad Fareed <muhammed.fareed.94@gmail.com>
2023-04-09 01:56:09 +0200

    Creating list item uimodel to use it in photos list

---
+++ PhotosRetriever/PhotosRetriever/PhotosModule/PhotosList/ListItem/ListItem.swift
@@ -0,0 +1,18 @@
+//
+//  ListItem.swift
+//  PhotosRetriever
+//
+//  Created by Muhammad Fareed on 09/04/2023.
+//
+
+import Foundation
+
+struct ListItem {
+    enum Kind {
+        case photo
+        case ad
+    }
+    var kind: Kind
+    let adMessage: String = "This is an ad"
+    var photo: Photo
+}
