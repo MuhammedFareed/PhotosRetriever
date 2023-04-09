31a6da911fdc30e06879147b6f17a83875d0244b
Muhammad Fareed <muhammed.fareed.94@gmail.com>
2023-04-09 06:08:13 +0200

    Creating photo details presenter

--- PhotosRetriever/PhotosRetriever/PhotosModule/PhotoDetails/Presenter/PhotoDetailsPresenter.swift
+++ PhotosRetriever/PhotosRetriever/PhotosModule/PhotoDetails/Presenter/PhotoDetailsPresenter.swift
@@ -8,21 +8,15 @@
 import UIKit
 
 protocol PhotosDetailsPresenterProtocol: AnyObject {
-//    func attachView(_ view: PhotosListViewControllerProtocol)
     func detectColorIn(_ image: UIImage) -> UIColor?
 }
 
 class PhotosDetailsPresenter: PhotosDetailsPresenterProtocol {
     private let colorDetector: DominantColorDetectorProtocol
-//    private weak var view: PhotosListViewControllerProtocol?
     
     init(withDetector detector: DominantColorDetectorProtocol = DominantColorDetector()) {
         self.colorDetector = detector
     }
-    
-//    func attachView(_ view: PhotosListViewControllerProtocol) {
-//        self.view = view
-//    }
     
     func detectColorIn(_ image: UIImage) -> UIColor? {
         return colorDetector.getDominantColor(from: image)
