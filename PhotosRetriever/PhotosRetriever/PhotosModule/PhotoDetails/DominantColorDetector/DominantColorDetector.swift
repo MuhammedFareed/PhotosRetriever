e5ecfcd2075ba0821baf6c1799586fc081b651a2
Muhammad Fareed <muhammed.fareed.94@gmail.com>
2023-04-09 06:01:14 +0200

    Creating a Dominant Color Detector

---
+++ PhotosRetriever/PhotosRetriever/PhotosModule/PhotoDetails/DominantColorDetector/DominantColorDetector.swift
@@ -0,0 +1,26 @@
+//
+//  DominantColorDetector.swift
+//  PhotosRetriever
+//
+//  Created by Muhammad Fareed on 09/04/2023.
+//
+
+import UIKit
+
+protocol DominantColorDetectorProtocol: AnyObject {
+    func getDominantColor(from image: UIImage) -> UIColor?
+}
+
+class DominantColorDetector: DominantColorDetectorProtocol {
+    func getDominantColor(from image: UIImage) -> UIColor? {
+        guard let inputImage = CIImage(image: image) else { return nil }
+        let extentVector = CIVector(x: inputImage.extent.origin.x, y: inputImage.extent.origin.y, z: inputImage.extent.size.width, w: inputImage.extent.size.height)
+        guard let filter = CIFilter(name: "CIAreaAverage", parameters: [kCIInputImageKey: inputImage, kCIInputExtentKey: extentVector]) else { return nil }
+        guard let outputImage = filter.outputImage else { return nil }
+        var bitmap = [UInt8](repeating: 0, count: 4)
+        let context = CIContext(options: nil)
+        context.render(outputImage, toBitmap: &bitmap, rowBytes: 4, bounds: CGRect(x: 0, y: 0, width: 1, height: 1), format: .RGBA8, colorSpace: nil)
+        return UIColor(red: CGFloat(bitmap[0]) / 255.0, green: CGFloat(bitmap[1]) / 255.0, blue: CGFloat(bitmap[2]) / 255.0, alpha: 1.0)
+        
+    }
+}
