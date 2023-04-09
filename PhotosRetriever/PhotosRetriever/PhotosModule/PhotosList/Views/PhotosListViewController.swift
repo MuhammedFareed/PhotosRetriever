75cb940db8470045bcfdd4303209da0bbc0ef743
Muhammad Fareed <muhammed.fareed.94@gmail.com>
2023-04-09 02:48:34 +0200

    Creating phots list view

---
+++ PhotosRetriever/PhotosRetriever/PhotosModule/PhotosList/Views/PhotosListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotosListViewController.swift
+//  PhotosRetriever
+//
+//  Created by Muhammad Fareed on 09/04/2023.
+//
+
+import UIKit
+
+class PhotosListViewController: UIViewController {
+
+    @IBOutlet weak var photosListTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
