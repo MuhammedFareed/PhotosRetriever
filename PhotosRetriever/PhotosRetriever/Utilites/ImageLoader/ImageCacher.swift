51db94aab1888411bd503b383a67ec6c48aeee82
Muhammad Fareed <muhammed.fareed.94@gmail.com>
2023-04-09 05:32:34 +0200

    Creating ImageCacher class

---
+++ PhotosRetriever/PhotosRetriever/Utilites/ImageLoader/ImageCacher.swift
@@ -0,0 +1,30 @@
+//
+//  ImageCacher.swift
+//  PhotosRetriever
+//
+//  Created by Muhammad Fareed on 09/04/2023.
+//
+
+import UIKit
+
+protocol ImageCacherProtocol {
+    func getImage(for url: URL) -> UIImage?
+    func setImage(_ image: UIImage, for url: URL)
+}
+
+class ImageCacher: ImageCacherProtocol {
+    
+    static let shared = ImageCacher()
+    
+    private let cache = NSCache<NSURL, UIImage>()
+    
+    private init() {}
+    
+    func getImage(for url: URL) -> UIImage? {
+        return cache.object(forKey: url as NSURL)
+    }
+    
+    func setImage(_ image: UIImage, for url: URL) {
+        cache.setObject(image, forKey: url as NSURL)
+    }
+}
